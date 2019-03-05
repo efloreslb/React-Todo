eb171236bbdbcc493f111c018947b762a10b2568
Edgar F <eflores.ef@gmail.com>
2019-03-04 21:27:20 -0800

    Initial commit. Set up import/export and class skeletons for each component.

---
+++ src/components/TodoComponents/Todo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Todo extends React.Component {
+    constructor() {
+        super();
+        
+    }
+
+    render() {
+        return (
+            <div>This is the Todo Component</div>
+        )
+    }
+}
+
+export default Todo;
---
+++ src/components/TodoComponents/TodoForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class TodoForm extends React.Component {
+    constructor() {
+        super();
+        
+    }
+
+    render() {
+        return (
+            <div>This is the TodoForm Component</div>
+        )
+    }
+}
+
+export default TodoForm;
